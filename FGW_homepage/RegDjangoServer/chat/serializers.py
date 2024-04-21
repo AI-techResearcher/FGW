7fa8b1a3f55483c3293b5ca79a0e43e57466609b
Zaland Afridi <Zaland.afridi5@gmail.com>
2024-04-21 21:38:46 +0500

    changes

--- FGW_homepage/RegDjangoServer/chat/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from exceptions import  Exception409,Exception400
-from .models import Chat
-
-class ChatSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Chat
-        fields = "__all__"
-        
